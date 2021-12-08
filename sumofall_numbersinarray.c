1fc029623ae9a262f24c2b75ba38caa18bc421d0
amangla612 <94043586+amangla612@users.noreply.github.com>
2021-12-08 22:15:52 +0530

    Create sumofall_numbersinarray.c

---
+++ sumofall_numbersinarray.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int arr[]={10,20,30,56,48,52,96};
+    int sum=0,i;
+    for(i=0;i<7;i++){
+        sum=sum+arr[i];
+    }
+    printf("***************+++++++++++++++********************\n");
+    printf(" The sum of array is %d\n",sum);
+    printf("\n$$$$$$$$$$$$$$$$###############&&&&&&&&&&&&&&&&&&");
+    
+
+    return 0;
+}
